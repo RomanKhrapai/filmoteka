1e2a279694966c001e12ecd0cd039333fa5a36b9
Yuliia Pysarenko <yuliapysarenko@gmail.com>
2022-01-14 11:33:45 +0200

    структуризує файли JS

--- src/js/header.js
+++ src/js/header.js
@@ -1,8 +1,3 @@
-export const btnHome = document.querySelector('.site-nav__button');
-export const btnLibrary = document.querySelector('.site-nav').lastElementChild.firstElementChild;
-export const searchButton = document.querySelector('.main-header__search-button');
-export const form = document.querySelector('.main-header__form');
-
 export function onBtnHomeClick(event) {
     document.location = 'page/index.html';
     event.currentTarget.classList('.is-active');
