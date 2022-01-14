9c3501609863e5e4d6c1d22194b9a7f59b6a409a
RomanKhrapai <hrapayr@gmail.com>
2022-01-14 18:16:18 +0200

    Конфлікти

--- src/js/refs.js
+++ src/js/refs.js
@@ -5,5 +5,5 @@ const btnHome = document.querySelector('.site-nav__button');
 const btnLibrary = document.querySelector('.site-nav').lastElementChild.firstElementChild;
 const searchButton = document.querySelector('.main-header__search-button');
 const form = document.querySelector('.main-header__form');
-
-export {btnHome, btnLibrary, searchButton, form};
+const galleryContainer = document.querySelector(".js-gallery");
+export {btnHome, btnLibrary, searchButton, form, galleryContainer};
