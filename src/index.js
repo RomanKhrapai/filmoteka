aba7691706c1ec9cad52b168f9d8c4dc260a6608
RomanKhrapai <hrapayr@gmail.com>
2022-01-14 12:02:08 +0200

    правлення структури

--- src/index.js
+++ src/index.js
@@ -1,34 +1,17 @@
 import './sass/main.scss';
-//------------
-const btnHome = document.querySelector('.site-nav__button')
-const btnLibrary = document.querySelector('.site-nav').lastElementChild.firstElementChild
-const searchButton = document.querySelector('.main-header__search-button')
-const form = document.querySelector('.hero__form')
 
-btnHome.addEventListener('click', onBtnHomeClick)
-btnLibrary.addEventListener('click', onBtnLibraryClick)
-form.addEventListener('submit', onFormSubmit)
 
-function onBtnHomeClick(event) {
 
-  btnHome.classList.add('is-active')  
-  btnLibrary.classList.remove('is-active')  
-}
-
-function onBtnLibraryClick(event) {
-   btnHome.classList.remove('is-active')
-    btnLibrary.classList.add('is-active')  
-}
- //----------
 
 
 // локальні імпорти
 import { btnHome, btnLibrary, searchButton, form } from "../src/js/refs.js";
 import { checkAuth, userSignOut } from '../src/js/auth.js';
-import { onBtnHomeClick, onFormSubmit } from '../src/js/header.js';
+import { onBtnHomeClick, onFormSubmit, onBtnLibraryClick } from '../src/js/header.js';
 
 checkAuth();
 userSignOut(); // можна виключити цю функцію, щоб не авторизовуватись після кожного оновлення сторінки
 
 btnHome.addEventListener('click', onBtnHomeClick);
 form.addEventListener('submit', onFormSubmit);
+btnLibrary.addEventListener('click', onBtnLibraryClick)
--- src/js/header.js
+++ src/js/header.js
@@ -1,10 +1,15 @@
 export function onBtnHomeClick(event) {
-    document.location = 'page/index.html';
-    event.currentTarget.classList('.is-active');
+    btnHome.classList.add('is-active')  
+  btnLibrary.classList.remove('is-active')  
 }
 
 export function onFormSubmit(event) {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
     event.currentTarget.reset();
+}
+
+export function onBtnLibraryClick(event) {
+   btnHome.classList.remove('is-active')
+    btnLibrary.classList.add('is-active')  
 }
