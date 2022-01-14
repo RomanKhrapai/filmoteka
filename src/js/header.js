abb2ed290a0b0e8d5bdefc9443625b741fee3dde
Iris2030 <ibazhinova@gmail.com>
2022-01-14 14:52:53 +0200

    header/ hero done

--- src/js/header.js
+++ src/js/header.js
@@ -1,6 +1,9 @@
 export function onBtnHomeClick(event) {
     btnHome.classList.add('is-active')  
-  btnLibrary.classList.remove('is-active')  
+  btnLibrary.classList.remove('is-active') 
+  homePageRender()
+ btnWatched.classList.remove('is-active-btn')
+    btnQueue.classList.remove('is-active-btn')
 }
 
 export function onFormSubmit(event) {
@@ -11,5 +14,36 @@ export function onFormSubmit(event) {
 
 export function onBtnLibraryClick(event) {
    btnHome.classList.remove('is-active')
-    btnLibrary.classList.add('is-active')  
+  btnLibrary.classList.add('is-active')  
+   headerHeroWrapper.classList.remove('header-hero__wrapper')
+  headerHeroWrapper.classList.add('header-hero__library-wrapper')
+  heroList.classList.remove('is-hidden')
+  form.classList.add('is-hidden')
 }
+
+ export function onBtnWatchedClick(event) {
+   btnWatched.classList.add('is-active-btn')
+    btnQueue.classList.remove('is-active-btn')
+} 
+
+ export function onBtnQueueClick(event) {
+   btnQueue.classList.add('is-active-btn')
+    btnWatched.classList.remove('is-active-btn')
+} 
+
+export function onHeaderButtonClick() {
+  homePageRender()
+  btnHome.classList.remove('is-active')
+  btnLibrary.classList.remove('is-active') 
+   btnWatched.classList.remove('is-active-btn')
+    btnQueue.classList.remove('is-active-btn')
+}
+
+export function homePageRender() {
+    headerHeroWrapper.classList.add('header-hero__wrapper')
+  headerHeroWrapper.classList.remove('header-hero__library-wrapper')
+  heroList.classList.add('is-hidden')
+  form.classList.remove('is-hidden')
+}
+
+import { btnHome, btnLibrary, searchButton, form, btnWatched, btnQueue, headerHeroWrapper,heroList,headerButton} from "./refs.js";
