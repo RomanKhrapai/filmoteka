19d57a57961965c5896052b617490a2cba99ce6a
Iris2030 <ibazhinova@gmail.com>
2022-01-15 19:57:51 +0200

    Moveis search added

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import './sass/main.scss';
 
 
 
-import { renderMarkup } from '../src/js/markup';
+import { renderMarkup,onFormSubmit} from '../src/js/markup';
 // локальні імпорти
 import { btnHome, btnLibrary, searchButton, form, btnWatched, btnQueue, headerHeroWrapper,heroList,headerButton} from "../src/js/refs.js";
 import { checkAuth, userSignOut } from '../src/js/auth.js';
-import { onBtnHomeClick, onFormSubmit, onBtnLibraryClick,onBtnWatchedClick, onBtnQueueClick,onHeaderButtonClick,homePageRender} from '../src/js/header.js';
+import { onBtnHomeClick, onBtnLibraryClick,onBtnWatchedClick, onBtnQueueClick,onHeaderButtonClick,homePageRender,fetchMovies} from '../src/js/header.js';
 
 checkAuth();
 userSignOut(); // можна виключити цю функцію, щоб не авторизовуватись після кожного оновлення сторінки
@@ -19,4 +19,3 @@ btnLibrary.addEventListener('click', onBtnLibraryClick)
 btnWatched.addEventListener('click', onBtnWatchedClick)
 btnQueue.addEventListener('click', onBtnQueueClick)
 headerButton.addEventListener('click', onHeaderButtonClick)
-
